d1cccff14fe3358f543f6dddd31288e13764f86c
Cao Minh Lam <caominhlam30395@gmail.com>
2017-06-14 23:05:23 +0700

    email regex

--- src/javastring/TestEmailRegex.java
+++ src/javastring/TestEmailRegex.java
@@ -1,14 +1,31 @@
 package javastring;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class TestEmailRegex {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
-        String PHONE_REGEX = "^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$";
-        String phone = "0966623949";
-        boolean check = phone.matches(PHONE_REGEX);
-        
-        System.out.println(check);
+
+        ArrayList<String> emails = new ArrayList<String>();
+        emails.add("sairamkrishna@tutorialspoint.com");
+        emails.add("kittuprasad700@gmail.com");
+        emails.add("sairamkrishna_mammahe%google-india.com");
+        emails.add("sairam.krishna@gmail-indai.com");
+        emails.add("sai#@youtube.co.in");
+        emails.add("kittu@domaincom");
+        emails.add("kittu#gmail.com");
+        emails.add("@pindom.com");
+
+        String regex = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+        Pattern pattern = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+        for (Object email : emails) {
+            Matcher matcher = pattern.matcher((CharSequence) email);
+            System.out.println(email + " : " + matcher.matches());
+        }
     }
 
 }
